f44a3643d57c9708a0a8a5a144de7fc01023687d
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2020-05-23 10:49:52 +0200

    get data from jokeData & map it to an array then render array of components

--- src/App.js
+++ src/App.js
@@ -1,41 +1,50 @@
 import React from "react";
-import Joke from "./components/Joke"
+import Joke from "./components/Joke";
+import jokesData from "./components/jokesData";
+console.log(jokesData);
 
 function App() {
+  const jokesArray = jokesData.map((joke) => (
+    <Joke question={joke.question} punchLine={joke.punchLine} key={joke.id} />
+  ));
+  console.log("App -> jokesArray", jokesArray);
   return (
     <div>
       <h1 className="mainHeader">Joke App</h1>
-      <Joke 
-      joke={{
-        question: "What's the difference between a married man and a bachelor?",
-        punchLine: "A bachelor will go to the fridge, sees nothing he wants, and go to bed A married man will go the bed..."
-      }}
+      {jokesArray}
+      <Joke
+        joke={{
+          question:
+            "What's the difference between a married man and a bachelor?",
+          punchLine:
+            "A bachelor will go to the fridge, sees nothing he wants, and go to bed A married man will go the bed...",
+        }}
       />
-      <Joke 
-      joke={{
-        question: "What's the difference between an epileptic oyster shucker and a hooker with diarrhea?",
-        punchLine: "One shucks between fits"
-      }}
+      <Joke
+        joke={{
+          question:
+            "What's the difference between an epileptic oyster shucker and a hooker with diarrhea?",
+          punchLine: "One shucks between fits",
+        }}
       />
-      <Joke 
-      joke={{
-        question: "Do you like fish sticks?",
-        punchLine: "Well then, you're a gay fish."
-      }}
+      <Joke
+        joke={{
+          question: "Do you like fish sticks?",
+          punchLine: "Well then, you're a gay fish.",
+        }}
       />
-      <Joke 
-      joke={{
-        question: "What is a pirate's worst nightmare?",
-        punchLine: "A sunken chest with no booty."
-      }}
+      <Joke
+        joke={{
+          question: "What is a pirate's worst nightmare?",
+          punchLine: "A sunken chest with no booty.",
+        }}
       />
-      <Joke 
-      joke={{
-        question: "What is statistically three times worse than a war?",
-        punchLine: "Three wars"
-      }}
+      <Joke
+        joke={{
+          question: "What is statistically three times worse than a war?",
+          punchLine: "Three wars",
+        }}
       />
-
     </div>
   );
 }
--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,17 +1,25 @@
 import React from "react";
-
-function Joke(props){
-    const style = {
-        fontSize: "1.5rem",
-        padding: "15px",
-        margin: 0
-    }
-    return(
-        <div className="jokeComponent">
-            <p style={style}>q: {props.joke.question}</p>
-            <p style={style}>a: {props.joke.punchLine}</p>
-        </div>
-    )
+function Joke(props) {
+  const style = {
+    fontSize: "1.5rem",
+    padding: "15px",
+    margin: 0,
+  };
+  return (
+    <div className="jokeComponent">
+      <p
+        className="jokeQuestion"
+        style={{
+          display: props.question
+            ? "block"
+            : "none" /* dont show empty question only punchLine*/,
+        }}
+      >
+        q: {props.question}
+      </p>
+      <p style={style}>a: {props.punchLine}</p>
+    </div>
+  );
 }
 
-export default  Joke;
+export default Joke;
