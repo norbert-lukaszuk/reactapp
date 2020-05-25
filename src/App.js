b2d137a408916c20462c9ee95507cdc6797b738b
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2020-05-25 13:57:29 +0200

    chanege login status in component & added some styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from "react";
-
-function App() {
-  return (
-    <div>
-      <h1 className="mainHeader">Hello from React</h1>
-    </div>
-  );
+import React, { Component } from "react";
+// example of using state to change rendered compononets values
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoggedIn: false,
+    };
+  }
+  render() {
+    let status;
+    let style = {
+      fontSize: "1em",
+    };
+    if (this.state.isLoggedIn) {
+      status = "login";
+      style.color = "green";
+    } else {
+      status = "logout";
+      style.color = "red";
+    }
+    return (
+      <h2 style={{ backgroundColor: "gray" }}>
+        User is <span style={style}>{status}</span>
+      </h2>
+    );
+  }
 }
 
 export default App;
