6109ec33092a7d849b95a6108002f4d0b831898b
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2020-05-25 15:14:02 +0200

    chenge components to class components tranfer data from props to state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import TodoItem from "./components/TodoItem";
 import Header from "./components/Header";
-function App() {
-  return (
-    <div className="wraper">
-      <Header />
-      <ul className="list">
-        <li>
-          <TodoItem />
-          <TodoItem />
-          <TodoItem />
-        </li>
-      </ul>
-    </div>
-  );
-}
+import todosData from "./components/todosData";
+class App extends Component {
+  render() {
+    const TodosArray = todosData.map((e) => (
+      <TodoItem key={e.id} text={e.text} completed={e.completed} />
+    ));
 
+    return (
+      <div className="wraper">
+        <Header />
+        <div className="list">{TodosArray}</div>
+      </div>
+    );
+  }
+}
 export default App;
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 
-function TodoItem() {
-  const SpanStyle = {
-    color: "green",
-    fontSize: "1.5rem",
-    paddingLeft: "15px",
-  };
-  const CheckboxStyle = {
-    width: "20px",
-    height: "20px",
-  };
-  return (
-    <div className="TodoItem">
-      <input type="checkbox" style={CheckboxStyle} />
-      <span style={SpanStyle}>Buy milk</span>
-    </div>
-  );
+class TodoItem extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    const { id, text, completed } = this.props;
+    this.state.id = id;
+    this.state.text = text;
+    this.state.completed = completed;
+    const SpanStyle = {
+      color: "green",
+      fontSize: "1.5rem",
+      paddingLeft: "15px",
+    };
+    const CheckboxStyle = {
+      width: "20px",
+      height: "20px",
+    };
+    return (
+      <div className="TodoItem">
+        <input
+          type="checkbox"
+          style={CheckboxStyle}
+          checked={this.state.completed}
+        />
+        <span style={SpanStyle}>{this.state.text}</span>
+      </div>
+    );
+  }
 }
-
 export default TodoItem;
