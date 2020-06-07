eb4c7521c93efeb77e357043c58135bb5b830b0e
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2020-06-07 12:56:25 +0200

    textarea

--- src/App.js
+++ src/App.js
@@ -7,29 +7,35 @@ class App extends React.Component {
       FirstName: "",
       LastName: "",
       Age: "",
+      Description: "",
     };
   }
   handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+    const { name, value } = e.target; // destructure properties name & value before asigne them to state
+    console.log(name, value);
+    this.setState({ [name]: value }); //input name attribute must be the same as key in state object
   };
   render() {
     return (
       <form>
         <input
           type="text"
-          name="FirstName"
+          value={this.state.FirstName} //this way we always know the state & input are in sync
+          name="FirstName" // this way we can handle few inputs using just one handle function
           placeholder="First Name"
           onChange={this.handleChange}
         />
         <br />
         <input
           type="text"
+          value={this.state.LastName}
           name="LastName"
           placeholder="Last Name"
           onChange={this.handleChange}
         />
         <input
           type="number"
+          value={this.state.Age}
           name="Age"
           placeholder="Age"
           onChange={this.handleChange}
@@ -39,6 +45,11 @@ class App extends React.Component {
           First Name: {this.state.FirstName} <br /> Last Name:{" "}
           {this.state.LastName} <br /> Age: {this.state.Age}
         </h4>
+        <textarea
+          name="description"
+          onChange={this.handleChange}
+          value={this.state.Description}
+        />
       </form>
     );
   }
