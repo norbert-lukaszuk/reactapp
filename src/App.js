e3a65156009f826327b2fe190fda8656ef13f838
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2020-07-24 13:27:45 +0200

    get quote and split author

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import GetQuote from "./components/GetQuote";
 
 /* Using useState hook to write App component as a function not class */
--- src/components/GetQuote.js
+++ src/components/GetQuote.js
@@ -1,15 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const GetQuote = () => {
   const [quote, setQuote] = useState("");
-  setQuote(
+
+  const handleClick = () => {
     fetch("https://quote-garden.herokuapp.com/api/v2/quotes/random")
       .then((resp) => resp.json())
-      .then((quote) => quote.quote.quoteText)
-  );
+      .then((data) => setQuote(data.quote));
+  };
+  const handleAuthorClick = (e) => {
+    const arr = e.target.textContent.split(" ");
+    console.log(arr);
+
+    // fetch("https://quote-garden.herokuapp.com/api/v2/authors/?page=1&limit=10")
+    //   .then((resp) => resp.json())
+    //   .then((data) => console.log(data));
+    console.log(e.target.textContent);
+  };
+  useEffect(handleClick, []);
   return (
     <div>
-      <p>{quote}</p>
+      <button onClick={handleClick}>Randome quote</button>
+      <p>{quote.quoteText}</p>
+      <p onClick={handleAuthorClick}>{quote.quoteAuthor}</p>
     </div>
   );
 };
