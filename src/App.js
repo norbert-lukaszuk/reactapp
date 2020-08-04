f05e991db794c8445e00d02161d6d4b43dac67c9
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2020-08-04 13:16:26 +0200

    next attempt to solve the problem

--- src/App.js
+++ src/App.js
@@ -2,20 +2,33 @@ import React, { useState, useEffect } from "react";
 import QuoteList from "./components/QuoteList";
 
 const App = () => {
-  const [quoteList, setQuoteList] = useState([]);
-  const randomQuote = () => {
-    fetch("https://quote-garden.herokuapp.com/api/v2/quotes/random")
-      // fetch("https://quote-garden.herokuapp.com/api/v2/quotes?page=1&limit=10")
-      .then((resp) => resp.json())
-      .then((data) => {
-        setQuoteList(...quoteList, data);
-      });
-    console.log(quoteList);
-  };
-  useEffect(randomQuote, []);
+  const quotes = [
+    {
+      id: 1,
+      quoteText:
+        "Stereotypes lose their power when the world is found to be more complex than the stereotype would suggest. When we learn that individuals do not fit the group stereotype, then it begins to fall apart.",
+      quoteAuthor: "Ed Koch",
+    },
+    {
+      id: 2,
+      quoteText: "We should weep for men at their birth, not at their death.",
+      quoteAuthor: "Charles de Montesquieu",
+    },
+  ];
+  const [quoteList, setQuoteList] = useState(quotes);
+  // const randomQuote = () => {
+  //   fetch("https://quote-garden.herokuapp.com/api/v2/quotes/random")
+  //     // fetch("https://quote-garden.herokuapp.com/api/v2/quotes?page=1&limit=10")
+  //     .then((resp) => resp.json())
+  //     .then((data) => {
+  //       setQuoteList(...quoteList, data);
+  //     });
+  //   console.log(quoteList);
+  // };
+  // useEffect(randomQuote, []);
   return (
     <div>
-      <button onClick={randomQuote}>Random Quote</button>
+      <button>Random Quote</button>
       <QuoteList quoteList={quoteList} />
     </div>
   );
--- src/components/QuoteList.js
+++ src/components/QuoteList.js
@@ -3,13 +3,19 @@ import { queryByPlaceholderText } from "@testing-library/react";
 
 export default function QouteList({ quoteList }) {
   let quotes = [];
+  const handleClick = () => {};
   if (quoteList) {
     console.log(quoteList);
-    // quotes = quoteList.map((quote) => {
-    //   return <p key={quote._id}>{quote.quoteText}</p>;
-    // });
-    // console.log(quotes);
-    // quotes = quoteList.quoteText;
+    quotes = quoteList.map((quote) => {
+      return (
+        <div key={quote.id}>
+          <p>{quote.quoteText}</p>
+          <p onClick={handleClick} style={{ color: "orange" }}>
+            {quote.quoteAuthor}
+          </p>
+        </div>
+      );
+    });
   }
   return <div>{quotes}</div>;
 }
