498e29e30f7afbac210ee2685920253388b86e0f
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2020-08-04 10:55:53 +0200

    I cant come up with solution

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import React, { useState, useEffect } from "react";
 import QuoteList from "./components/QuoteList";
 
 const App = () => {
+  const [quoteList, setQuoteList] = useState([]);
+  const randomQuote = () => {
+    fetch("https://quote-garden.herokuapp.com/api/v2/quotes/random")
+      // fetch("https://quote-garden.herokuapp.com/api/v2/quotes?page=1&limit=10")
+      .then((resp) => resp.json())
+      .then((data) => {
+        setQuoteList(...quoteList, data);
+      });
+    console.log(quoteList);
+  };
+  useEffect(randomQuote, []);
   return (
     <div>
-      <button>Randome Quote</button>
-      <QuoteList />
+      <button onClick={randomQuote}>Random Quote</button>
+      <QuoteList quoteList={quoteList} />
     </div>
   );
 };
--- src/components/QuoteList.js
+++ src/components/QuoteList.js
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
+import { queryByPlaceholderText } from "@testing-library/react";
 
-export default function QouteList() {
-  return (
-    <div>
-      <p>Hello React</p>
-    </div>
-  );
+export default function QouteList({ quoteList }) {
+  let quotes = [];
+  if (quoteList) {
+    console.log(quoteList);
+    // quotes = quoteList.map((quote) => {
+    //   return <p key={quote._id}>{quote.quoteText}</p>;
+    // });
+    // console.log(quotes);
+    // quotes = quoteList.quoteText;
+  }
+  return <div>{quotes}</div>;
 }
