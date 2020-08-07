57222afd8867526b946aae6484f6fd6b3c64895e
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2020-08-07 21:32:00 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React, { useState } from "react";
-import Counter from "./components/Counter";
-import Joke from "./components/Joke";
-import Checkbox from "./components/Checkbox";
-import Login from "./components/Login";
-import Logout from "./components/Logout";
-import CheckUser from "./components/CheckUser";
-import InputField from "./components/InputField";
+
 /* Using useState hook to write App component as a function not class */
 const App = () => {
   return (
     <div>
-      <Counter start={0} increment={25} />
-      <Counter start={0} increment={3} />
-      <Joke />
-      <Checkbox />
-      <Checkbox />
-      <Checkbox />
-      <InputField />
-      <CheckUser />
-      <Logout />
+      <h3>Hello from react</h3>
     </div>
   );
 };
